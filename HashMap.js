6bcc333429c5f0005f52a48d8dc8afbb61b9b7ed
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-07-27 14:08:40 +0530

    Add HashMap class with basic set, get, and hash methods.

---
+++ HashMap.js
@@ -0,0 +1,50 @@
+class HashMap {
+  constructor(initialCapacity = 16, loadFactor = 0.75) {
+    this.buckets = new Array(initialCapacity);
+    this.loadFactor = loadFactor;
+    this.size = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
+    }
+    return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      this.buckets[index] = [];
+    }
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        return;
+      }
+    }
+    bucket.push([key, value]);
+    this.size++;
+    if (this.size / this.buckets.length > this.loadFactor) {
+      this.resize();
+    }
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          return bucket[i][1];
+        }
+      }
+    }
+    return null;
+  }
+}
+
+
